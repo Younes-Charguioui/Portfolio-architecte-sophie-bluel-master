669bf3ee33c30ae8e89898bef9cafd213fad16ca
Younes-Charguioui <72479723+Younes-Charguioui@users.noreply.github.com>
2023-05-25 10:13:20 +0200

    Login | Token | Modals
    implémentation presque complète de presque tous.

--- FrontEnd/assets/js/app.js
+++ FrontEnd/assets/js/app.js
@@ -12,6 +12,9 @@ let modal = null
 let user = {
 	token: null
 }
+let cibleModal = null
+let removedArticleId = []
+let formState = false
 
 if (sessionStorage.getItem('token') != null) {
 	user.token = sessionStorage.getItem('token')
@@ -123,11 +126,38 @@ function accessAdmin() {
 function openModal(e) {
 	e.preventDefault()
 	modal = document.querySelector(e.target.getAttribute('href'))
-	modal.style.display = null
 	modal.removeAttribute('aria-hidden')
 	modal.setAttribute('aria-modal','true')
 	modal.addEventListener('click',closeModal)
-	document.getElementById('js-modal-close').addEventListener('click',closeModal)
+	if (modal.getAttribute('id') === 'modal') {
+		elementModal()
+	}
+	if (modal.getAttribute('id') === 'modal-adding') {
+		const div = document.getElementById("div-file")
+		div.innerHTML = ""
+		div.innerHTML = "<i class='fa-regular fa-image'></i><form><input type='file' id='file' name='file' accept='image/png, image/jpeg'><label for='file'>+ Ajouter photo</label></form><span>jpg, png : 4mo max</span>"
+		const input = document.getElementById("file")
+		input.addEventListener("change",(e) => {
+			const div = document.getElementById("div-file")
+			div.innerHTML = ""
+			const img = document.createElement('img')
+			img.src = URL.createObjectURL(input.files[0])
+			img.alt = "image utilisateur"
+
+			div.appendChild(img)
+		}, false)
+		document.getElementById('title').value = ""
+		document.getElementById('category').value = ""
+		document.getElementById('category').innerHTML = ""
+		filtres.forEach(item => {
+			const option = document.createElement('option')
+			option.value = item
+			option.text = item
+			document.getElementById('category').add(option)
+		})
+	}
+	modal.style.display = null
+	modal.querySelector('#js-modal-close').addEventListener('click',closeModal)
 	modal.querySelector('.modal-content').addEventListener('click',(e) => e.stopPropagation())
 }
 
@@ -138,7 +168,143 @@ function closeModal(e) {
 	modal.setAttribute('aria-hidden','true')
 	modal.removeAttribute('aria-modal')
 	modal.removeEventListener('click',closeModal)
+	if (modal.getAttribute('id') === 'modal-adding') {
+		modal = document.querySelector('#modal')
+		modal.style.display = "none"
+		modal.setAttribute('aria-hidden','true')
+		modal.removeAttribute('aria-modal')
+		modal.removeEventListener('click',closeModal)
+	}
 	modal = null
+	removedArticleId = []
+}
+
+function returnModal(e) {
+	if (modal === null) return
+	e.preventDefault()
+	modal.style.display = "none"
+	modal.setAttribute('aria-hidden','true')
+	modal.removeAttribute('aria-modal')
+	modal.removeEventListener('click',closeModal)
+	modal = document.querySelector('#modal')
 }
 
 document.getElementById("js-open-modal").addEventListener('click', openModal)
+document.getElementById("js-open-modal-adding").addEventListener('click', openModal)
+document.getElementById("js-modal-return").addEventListener('click', returnModal)
+
+function elementModal() {
+	const div = document.getElementById('modal-cards')
+	div.innerHTML = ""
+
+	allWorks.forEach((item, id) => {
+		const article = document.createElement('article')
+		const img = document.createElement('img')
+		const span = document.createElement('span')
+		const divGroup = document.createElement('div')
+		const divIcon = document.createElement('div')
+		const trashIcon = document.createElement('i')
+
+		trashIcon.setAttribute('class','fa-regular fa-trash-can')
+		trashIcon.setAttribute('onclick',`deleteArticleModal(${id})`)
+		divIcon.setAttribute('class','card-icon')
+		divIcon.appendChild(trashIcon)
+		divGroup.setAttribute('class','card-icon-group')
+		if (id === 0) {
+			const divFirstIcon = document.createElement('div')
+			const firstIcon = document.createElement('i')
+			firstIcon.setAttribute('class','fa-solid fa-arrows-up-down-left-right')
+			divFirstIcon.setAttribute('class','card-icon')
+			divFirstIcon.appendChild(firstIcon)
+			divGroup.appendChild(divFirstIcon)
+		}
+		divGroup.appendChild(divIcon)
+		img.alt = "une réalisation de Sophie Bruel"
+		img.src = item.url
+		span.appendChild(document.createTextNode('éditer'))
+		article.setAttribute('class','card')
+
+		article.appendChild(img)
+		article.appendChild(span)
+		article.appendChild(divGroup)
+		article.setAttribute('id',`article${id}`)
+
+		article.setAttribute('draggable','true')
+		img.setAttribute('draggable','false')
+		span.setAttribute('draggable','false')
+		article.setAttribute('ondrop','drop(event)')
+		article.setAttribute('ondragover','dragover(event)')
+		article.setAttribute('ondragstart','drag(event)')
+		div.appendChild(article)
+	})
+}
+
+//des qu'un element bouge de son emplacement
+function dragover(event) {
+	event.preventDefault()
+	let target = event.target
+	while (target.localName !== "article"){
+		target = target.parentNode
+	}
+	
+	target.style.border = "2px solid #000000"
+	target.addEventListener("dragleave", (event) => {
+	  target.style.border = null
+	});
+}
+
+//dès qu'un element bouge de son emplacement
+function drag(event) {
+	event.dataTransfer.setData("text/html",event.target.outerHTML)
+	cibleModal = event.target
+}
+
+function drop(event) {
+	event.preventDefault()
+	const data = event.dataTransfer.getData("text/html")
+	let target = event.target
+	while (target.localName !== "article"){
+		target = target.parentNode
+	}
+
+	cibleModal.outerHTML = target.outerHTML
+	target.outerHTML = data
+	let tmp = document.getElementsByTagName("article")
+	for (let item of tmp){
+		if (item.style.border != "") {
+			item.style.border = null
+		}
+	}
+	cibleModal = null
+}
+
+function deleteArticleModal(id) {
+	const article = document.getElementById(`article${id}`)
+	removedArticleId.push(id)
+	article.remove()
+}
+
+if (document.getElementById('title') != null && document.getElementById('category') != null) {
+	console.log('111111')
+	if (document.getElementById('file') != null) {
+		if (document.getElementById('file').files.length != 0) {
+			if (formState != true) {
+				console.log("ok many")
+				formState = true
+			}
+		}
+	}
+}
+
+
+
+/*function temp() {
+	let response = await fetch("/mail",{
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8',
+			'Authorization': 'Bearer ' + localStorage.getItem('token'),
+			'Authorization': `Bearer ${localStorage.getItem('token')}`
+		}
+	})
+}*/
